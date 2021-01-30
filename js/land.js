2c4f0e21171a9c7c74e00d2967f116cbce18932d
Vicente Constante <eduardo_constante_109@hotmail.com>
2021-01-30 17:33:50 -0500

    update pag 1

--- js/land.js
+++ js/land.js
@@ -1,14 +1,23 @@
 let prop = {
 
-    $body: document.getElementsByTagName("BODY")
+    $body: document.getElementsByTagName("BODY"),
+    $hamburger: document.getElementById("hambur"),
+    $menu:document.getElementById("menu"),
 }
 let met = {
 
-    inicio: function (valor) {
+    inicio: function () {
         if (document.documentElement.scrollTop > 700 && document.documentElement.scrollTop < 1200) {
         } else {
         }
     },
+    webup:function(){
+
+        prop.$hamburger.addEventListener("click", function(){
+            prop.$menu.style.transition = "all, 0.5s";
+            prop.$menu.style.right = "5px";
+        })
+    }
 
 }
 met.webup();
