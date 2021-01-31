81b83d39f7a25f7cbc073d825078b68d1c483702
Vicente Constante <eduardo_constante_109@hotmail.com>
2021-01-30 21:48:51 -0500

    js editado

--- js/land.js
+++ js/land.js
@@ -1,8 +1,7 @@
 let prop = {
-
-    $body: document.getElementsByTagName("BODY"),
     $hamburger: document.getElementById("hambur"),
-    $menu:document.getElementById("menu"),
+    $menu: document.getElementById("menu"),
+    varmenu: null
 }
 let met = {
 
@@ -11,12 +10,25 @@ let met = {
         } else {
         }
     },
-    webup:function(){
+    webup: function () {
+        prop.$hamburger.addEventListener("click", function (dato) {
+            prop.$hamburger.style.zIndex = "80";
+            do {
+                prop.$menu.style.transition = "all, 0.5s";
+                prop.$menu.style.right = "5px";
+                prop.varmenu == false;
+            } while (prop.varmenu == false);
+            console.log("salido del do",varmenu);
+            window.addEventListener("click", function (dato) {
+                console.log(prop.varmenu);
+                console.log("dentro");
+                if (prop.varmenu == false && dato.target.getAttribute("id") != "menu") {
+                    prop.$menu.style.right = "-300px";
+                }
+            });
 
-        prop.$hamburger.addEventListener("click", function(){
-            prop.$menu.style.transition = "all, 0.5s";
-            prop.$menu.style.right = "5px";
         })
+
     }
 
 }
