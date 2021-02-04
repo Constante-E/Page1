02754531df0a9a194b90f6cba984ef393c316997
Vicente Constante <eduardo_constante_109@hotmail.com>
2021-02-03 21:11:08 -0500

    progreso pagina

--- js/land.js
+++ js/land.js
@@ -1,21 +1,36 @@
 let prop = {
     $hamburger: document.getElementById("hambur"),
     $menu: document.getElementById("menu"),
+    $movoff: document.querySelectorAll(".movoff"),
 }
 let met = {
 
     inicio: function () {
-        if (document.documentElement.scrollTop > 700 && document.documentElement.scrollTop < 1200) {
-        } else {
+        let scrll = document.documentElement.scrollTop;
+        if (scrll > 700 && scrll < 1030) {
+            prop.$movoff[0].style.opacity = "1";
+            prop.$movoff[0].style.transform = "translateY(0px)";
+        }
+        else {
+            prop.$movoff[0].style.opacity = "0%";
+            prop.$movoff[0].style.transform = "translateY(100px)";
+        }
+        if (scrll > 1400 && scrll < 1770) {
+            prop.$movoff[1].style.opacity = "1";
+            prop.$movoff[1].style.transform = "translateY(0px)";
+        }
+        else {
+            prop.$movoff[1].style.opacity = "0%";
+            prop.$movoff[1].style.transform = "translateY(100px)";
         }
     },
     webup: function () {
         prop.$hamburger.addEventListener("click", met.menu);
     },
     menu: function () {
-        if(prop.$menu.style.right == "5px"){
+        if (prop.$menu.style.right == "5px") {
             prop.$menu.style.right = "-300px";
-        }else{
+        } else {
             prop.$menu.style.right = "5px";
         }
     }
